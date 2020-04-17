a2507ac537720db0ed0eddb284e30c6bde32a295
FosterGun <fostergun123@gmail.com>
2020-04-17 20:39:58 +0200

    Arreglos del modelo Media y añadido modelo Tag

--- gestionArticulos/admin.py
+++ gestionArticulos/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django import forms
 
-from gestionArticulos.models import Media
+from gestionArticulos.models import Media, Tag
 from gestionUsuarios.models import User
 
 # Register your models here.
 # Permite gestionar los datos que podrá manipular el administrador de la aplicación.
+
+class TagAdmin(admin.ModelAdmin):
+    """
+    Opciones de filtrado/operación para el modelo de información de los tags de los artículos multimedia.
+    """
+
+    list_display = ("name",)
+    search_fields = ("name",)
 
 class MediaAdmin(admin.ModelAdmin):
     """
@@ -16,9 +25,20 @@ class MediaAdmin(admin.ModelAdmin):
         if Media:
             return Media.propietario.username
 
-    list_display = ("nickpropietario", "nombre", "accion", "categoria", "tags", "fechaAd")
-    readonly_fields = ('fechaAd',)
-    search_fields = ("nickpropietario", "nombre", "tags", "fechaAd")
-    list_filter = ("accion", "categoria","fechaAd")
+    # Lista en un string separado por comas en el display los tags de un Media
+    def tagsnames(self, Media):
+        if Media:
+            return ','.join([str(tag.name) for tag in Media.tags.all()])
 
+    # Añade un textarea al formulario del administrador para la descripción de Media
+    def get_form(self, request, obj=None, **kwargs):
+        kwargs['widgets'] = {'descripcion': forms.Textarea}
+        return super().get_form(request, obj, **kwargs)
+
+    list_display = ("nickpropietario", "nombre", "tagsnames", "accion", "categoria", "fechaAd")
+    readonly_fields = ('fechaAd',)
+    search_fields = ("nombre", "fechaAd")
+    list_filter = ("accion", "categoria", "fechaAd")
+
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Media, MediaAdmin)
--- gestionArticulos/models.py
+++ gestionArticulos/models.py
@@ -6,6 +6,19 @@ from gestionArticulos.enums import AccionType, CategoriaType
 # Create your models here.
 # Los modelos serán las tablas de datos que tendrá nuestra base de datos
 
+class Tag(models.Model):
+    """
+        Modelo de datos para la información de los tags de los artículos multimedia de los usuarios.
+
+        Se compone de:
+        - Un string de 15 caracteres por lo menos con el tag en cuestión
+    """
+
+    name=models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
 class Media(models.Model):
     """
         Modelo de datos para la información de los artículos multimedia de los usuarios.
@@ -30,17 +43,12 @@ class Media(models.Model):
     fotoart=models.ImageField(upload_to='articulos')
     accion=models.CharField(max_length=12, choices=AccionType.choices(), verbose_name='Acción')
     categoria=models.CharField(max_length=12, choices=CategoriaType.choices(), verbose_name='Categoría')
-    tags=models.CharField(max_length=100)
+    tags=models.ManyToManyField(Tag, blank=True)
     asignado=models.ForeignKey(User, on_delete=models.SET_NULL, related_name='user_asignado', blank=True, null=True)
     fechaAd=models.DateTimeField(auto_now_add=True, verbose_name='Fecha de adición')
 
     def categoria_text(self):
         return CategoriaType[self.categoria].value
-    def tags_as_list(self):
-        return self.tags.split(',')
 
     def __str__(self):
         return "Media: %s con propietario %s, nombre %s y fecha de creación %s" % (self.media_id, self.propietario.username, self.nombre, self.propietario.date_joined)
-
-
-
